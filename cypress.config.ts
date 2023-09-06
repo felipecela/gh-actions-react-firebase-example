3c52589177712e7c55b9343136aaa809e45606ac
felipecela <fcela@student.threepoints.com>
2023-09-06 17:47:51 +0200

    test15-cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,23 @@ export default defineConfig({
   video: true,
   fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      on(
+        'after:spec',
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === 'failed')
+            )
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video)
+            }
+          }
+        }
+      )
+    },
     supportFile: false,
     baseUrl: 'http://localhost:3000',
     projectId: "rrpyro",
